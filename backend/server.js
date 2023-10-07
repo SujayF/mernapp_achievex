24e4d0d189597a4cc99eae4b306a0b24d7977cfa
Sujay <sujaygf@gmai.com>
2023-10-07 11:36:58 +0530

    headeup

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,12 @@ app.use(
   })
 )
 
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Replace * with specific domains
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
