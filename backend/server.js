e0bdb3a309e33cf654abc8139fd2648d550f3a05
Sujay <sujaygf@gmai.com>
2023-10-07 11:15:38 +0530

    commit-8th

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,11 @@ const app = express();
 //   })
 // )
 
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
